34162bb31a434f35c809f76e44d5b1d4879edc23
Devender <bishtdev293@gmail.com>
2025-07-04 10:57:24 +0530

    Change the logic of program from using positioning betore to now using transform for smoother animation.

--- script.js
+++ script.js
@@ -4,22 +4,20 @@ let no_btn = document.querySelector(".red");
 
 no_btn.addEventListener("click", () => {
     alert("This is not possible. Are you out of your mind!?😤")
-})
+});
 
 yes_btn.addEventListener("click", () => {
     prompt("Show your feelings in atleast 1lakh characters.🙈")
-})
-
+});
 
 no_btn.addEventListener("mouseover", () => {
-    let left = getRandomnumber(0, 100);
-    let top = getRandomnumber(50, 100);
+    const x = getRandomnumber(-300, 300);
+    const y = getRandomnumber(-300, 300);
+    no_btn.style.transform = `translate(${x}%, ${y}%)`;
+});
 
-    no_btn.style.left = `${left}%`;
-    no_btn.style.top = `${top}%`;
-})
 
 
 function getRandomnumber(min, max) {
     return Math.random() * (max - min) + min;
-}
+};
